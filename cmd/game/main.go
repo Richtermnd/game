2ca3a04b76a7b1ec26c877f7314b63c030ef1761
Richtermnd <m.rustam1305@gmail.com>
2024-07-27 00:10:44 +0500

    Animations logic.

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -4,12 +4,17 @@ import (
 	"log"
 
 	"github.com/Richtermnd/game/internal/game"
+	"github.com/Richtermnd/game/internal/player"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
 	log.Default().SetPrefix("")
 	g := game.New()
+	p1 := player.NewPlayer("olaf", 100, 100)
+	p2 := player.NewPlayer("cole", 200, 100)
+	p3 := player.NewPlayer("kira", 300, 100)
+	g.AddDrawer(p1, p2, p3)
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
---
+++ internal/player/player.go
@@ -0,0 +1,35 @@
+package player
+
+import (
+	"github.com/Richtermnd/game/internal/player/animations"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Player struct {
+	X, Y  int
+	anims *animations.AnimationSet
+}
+
+func NewPlayer(name string, x, y int) *Player {
+	anims := animations.New(name)
+	anims.SetState(animations.IDLE)
+	return &Player{
+		X:     x,
+		Y:     y,
+		anims: anims,
+	}
+}
+
+func (p *Player) Layout() int {
+	return 10
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	frame := p.anims.NextFrame()
+	w, h := frame.Bounds().Dx(), frame.Bounds().Dy()
+	ops := ebiten.DrawImageOptions{}
+	ops.GeoM.Scale(3, 3)
+	ops.GeoM.Translate(-float64(w/2), -float64(h/2))
+	ops.GeoM.Translate(float64(p.X), float64(p.Y))
+	screen.DrawImage(frame, &ops)
+}
